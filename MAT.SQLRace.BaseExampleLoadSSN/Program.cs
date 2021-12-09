6db58039aebd2b186fd0cbb00e75388f5fe49ee6
Elias Ibn Ziaten Cerezo <elias2603@hotmail.com>
2021-12-09 15:57:21 +0000

    Adding BaseLoadSSN and CreateSessionWithMultipleParameters Examples

---
+++ MAT.SQLRace.BaseExampleLoadSSN/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using MESL.SqlRace.Common;
+using MESL.SqlRace.Common.Extensions;
+using MESL.SqlRace.Domain;
+using MESL.SqlRace.Common.Extensions;
+
+
+
+/// <summary>
+/// In order to be able to use this example you need to register McLaren Applied NuGet repository in Visual Studio. Link below
+/// https://github.com/mat-docs/packages
+///
+/// Whenever you are setting up your project you should use .NETFramework 4.8.
+/// 
+/// Once registered in Visual Studio, you need to install MESL.SQLRace.API package from NuGet making sure that you have selected the McLaren Applied Github packages in the top
+/// right corner (defaults to nuget.org, where if you search for the package it would not show).
+/// Apart from this NuGet package, if you plan to use SSN files you need to follow these steps too:
+/// - Install MAT.ATLAS.SupportFiles from NuGet directory making sure that you have selected on the top right corner MA repository
+/// - Setup the build config to build in x64 based CPUs. This is required for SSNs to be loaded.
+/// Once this package is installed, you would only need to pay attenion to the TODOs written in the code.
+///
+/// Find the documentation for the API in the link below
+/// https://mat-docs.github.io/Atlas.SQLRaceAPI.Documentation/api/index.html
+/// 
+/// Further examples can be found here:
+/// https://github.com/mat-docs/MAT.OCS.SQLRace.Examples
+/// 
+/// </summary>
+
+namespace BaseExampleSSN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //TODO: Change the path to file to one sensible location
+            string pathToFile = @"C:\Users\YOUR_USER\SOME_FOLDER\SOME_FILE.ssn";
+
+            //Initialize SQLRaceAPI
+            Console.WriteLine("Initializing SQL Race....");
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Core.LicenceProgramName = "SQLRace";
+            Core.Initialize();
+
+            Console.WriteLine("SQLRace has been initialized correctly");
+            var clientSession = LoadSession(pathToFile);
+            ReadSamples(clientSession);
+
+            Console.WriteLine("Press enter to exit...");
+            Console.ReadLine();
+            Log.SetMinimumLoggingLevel(LogLevel.Warn);
+
+        }
+
+        private static void ReadSamples(IClientSession clientSession)
+        {
+            var vCarIdentifier = "vCar:Chassis";
+            using (var pda = clientSession.Session.CreateParameterDataAccess(vCarIdentifier))
+            {
+                pda.GoTo(clientSession.Session.StartTime + (clientSession.Session.EndTime - clientSession.Session.StartTime) / 2);
+                //MAX SAMPLES RETRIEVED: 10000
+                var samples = pda.GetNextSamples(10);
+
+                Console.WriteLine($"** Data for {vCarIdentifier}");
+                for (var i = 0; i < samples.SampleCount; i++)
+                {
+                    Console.WriteLine($"{samples.Timestamp[i].ToTimeString()} {samples.Data[i]}");
+                }
+            }
+        }
+
+        private static IClientSession LoadSession(string pathToFile)
+        {
+            var fileSessionManager = FileSessionManager.CreateFileSessionManager();
+            var clientSession = fileSessionManager.Load(pathToFile); // session with associates
+
+            if (clientSession == null)
+            {
+                Console.WriteLine("Session not found");
+                return null;
+            }
+
+            return clientSession;
+        }
+    }
+}
---
+++ MAT.SQLRace.CreateSessionWithMultipleParameters/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MAT.OCS.Core;
+using MESL.SqlRace.Common.Extensions;
+using MESL.SqlRace.Domain;
+using MESL.SqlRace.Domain.Infrastructure.DataPipeline;
+using MESL.SqlRace.Enumerators;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+
+namespace MAT.SQLRace.CreateSessionWIthMultipleParameters
+{
+    class Program
+    {
+        
+        public static IClientSession CreateBasicSession(string connectionString, string sessIdentifier)
+        {
+            SessionManager sessionManager = SessionManager.CreateSessionManager();
+            SessionKey sessionKey = SessionKey.NewKey();
+            IClientSession clientSession = sessionManager.CreateSession(connectionString, sessionKey, sessIdentifier,DateTime.Now, "Session");
+            return clientSession;
+        }
+
+        public static Dictionary<string, uint> PopulateSession(IClientSession clientSession, IEnumerable<string> identifers)
+        {
+            // Take into account that the identifiers inside the IEnumberable cannot contain spaces
+            Dictionary<string, uint> channels = new Dictionary<string, uint>();
+            //Constants for creating parameters
+            const string ConversionFunctionName = "CONV_MyParam:MyApp";
+            const string ApplicationGroupName = "MyApp";
+            const string ParameterGroupIdentifier = "MyParamGroup";
+
+            Session session = clientSession.Session;
+            ConfigurationSet config = session.CreateConfiguration();
+            // Creating the parameter group where the channels will be added later
+            ParameterGroup group1 = new ParameterGroup(ParameterGroupIdentifier, "SampleDescription");
+
+            // Application group without RDA where the parameter group will live
+            ApplicationGroup appGroup1 = new ApplicationGroup(
+                ApplicationGroupName,
+                ApplicationGroupName,
+                new List<string>
+                {
+                    group1.Identifier
+                })
+            {
+                SupportsRda = false
+            };
+
+            config.AddGroup(appGroup1);
+            config.AddParameterGroup(group1);
+            //Conversion function
+            config.AddConversion(
+                new RationalConversion(ConversionFunctionName, 
+                                    "kph", 
+                                    "%5.2f", 
+                                    0.0, 
+                                    1.0, 
+                                    0.0, 
+                                    0.0, 
+                                    0.0, 
+                                    1.0)
+                );
+            Frequency samplingFrequency = new Frequency(2, FrequencyUnit.Hz);
+            uint channelId = 1;
+            // We start to iterate over the identifiers to add the parameters to the param group
+            foreach (string identifier in identifers)
+            {
+                channels[identifier] = channelId;
+                Channel channel = new Channel(
+                    channelId++,
+                    "Channel" + channelId,
+                    samplingFrequency.ToInterval(),
+                    DataType.Signed16Bit,
+                    ChannelDataSourceType.Periodic);
+
+                Parameter parameter = new Parameter(
+                    identifier,
+                    identifier,
+                    identifier,
+                    400, // Your max
+                    0, // Your min
+                    1,
+                    0,
+                    0,
+                    255,
+                    0,
+                    ConversionFunctionName,
+                    new List<string>
+                    {
+                        ParameterGroupIdentifier
+                    },
+                    channel.Id,
+                    ApplicationGroupName);
+
+                config.AddChannel(channel);
+                config.AddParameter(parameter);
+            }
+
+            config.Commit();
+            return channels;
+
+        }
+
+        public static void FillSessionDetails(IClientSession clientSession, IEnumerable<string> details)
+        {
+            Session session = clientSession.Session;
+            // Populating session details
+            int i = 1;
+            foreach (var detail in details)
+            {
+                session.Items.Add(new SessionDataItem("Detail" + i, detail));
+                i++;
+            }
+
+        }
+
+        public static void AddSampleLap(IClientSession clientSession, long startTime, short number, byte triggersource, string name, bool countForFastestLap)
+        {
+            var session = clientSession.Session;
+
+            var lap = new Lap(startTime,number, triggersource, name, countForFastestLap);
+            // You can use the lap object to edit the different properties of it
+            //lap.EndTime = 1;
+
+            session.Laps.Add(lap);
+            //In case you would like to add data attached to the lap you can do the following
+            session.Laps.LapItems.Add(new LapDataItem(number, name, "Race start"));
+        }
+
+        static void Main(string[] args)
+        {
+            string connectionString = $@"DbEngine=SQLite;Data Source={
+                    Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)
+                }\McLaren Applied Technologies\ATLAS 10\SQL Race\test.db;";
+
+            Console.WriteLine("Initializing SQL Race....");
+            Core.LicenceProgramName = "SQLRace";
+            Core.Initialize();
+
+            IClientSession clientSession = CreateBasicSession(connectionString, "SampleIdentifier");
+            Console.WriteLine($"Creating session");
+
+            // Adding channels
+            List<string> identifiers = new List<string>
+            {
+                "sampleparam1",
+                "sampleparam2"
+            };
+
+            Dictionary<string, uint> channels = PopulateSession(clientSession, identifiers);
+
+            //Adding session details
+            List<string> details = new List<string>
+            {
+                "sampledetail1",
+                "sampledetail2"
+            };
+              
+            FillSessionDetails(clientSession, details);
+            //Adding a sample lap
+            AddSampleLap(clientSession,0,1,0,"Lap1", false);
+
+            Console.WriteLine("Press ENTER or RETURN key to finish...");
+            Console.ReadLine();
+        }
+    }
+}
