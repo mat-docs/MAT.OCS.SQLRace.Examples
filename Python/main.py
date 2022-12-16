504288065c8de88f532955e5aa6de696fdfe2589
Owen Foo <owen.foo@mclaren.com>
2022-12-16 13:31:33 +0000

    Add events example
    Use callbacks from Automation API to check if ATLAS is conncted and if session is loaded

--- Python/main.py
+++ Python/main.py
@@ -2,6 +2,7 @@ import os
 import time
 import clr # Pythonnet
 import subprocess
+import threading
 
 # The path to the main SQL Race DLL. This is the default location when installed with Atlas 10
 sql_race_dll_path = r"C:\Program Files\McLaren Applied Technologies\ATLAS 10\MESL.SqlRace.Domain.dll"
@@ -14,6 +15,7 @@ connection_string = r"Server=CLU-LDF01\LOCAL;Initial Catalog=SQLRACE01_2019;Trus
 # Configure Pythonnet and reference the required assemblies for dotnet and SQL Race
 clr.AddReference("System.Collections")
 clr.AddReference("System.Core")
+clr.AddReference("System.IO")
 
 if not os.path.isfile(sql_race_dll_path):
     raise Exception("Couldn't find SQL Race DLL at " + sql_race_dll_path + " please check that Atlas 10 is installed")
@@ -38,6 +40,8 @@ from MESL.SqlRace.Domain.Sort import *
 from MESL.SqlRace.Domain.Infrastructure.Enumerators import *
 from MAT.Atlas.Automation.Client.Services import *
 from MAT.Atlas.Automation.Api.Models import *
+from System.IO import Path
+from System import AppDomain
 
 keys = []
 
@@ -160,15 +164,44 @@ def set_cursor(keys, connection_strings, timestamp):
 
     # Open ATLAS10 from default installation location
     subprocess.Popen(r"C:\Program Files\McLaren Applied Technologies\ATLAS 10\MAT.ATLAS.Host.exe")
-    print("Waiting for ATLAS to open") # here, instead of sleep, callbacks from Automation API MAT.Atlas.Automation.Client.Services.ApplicationServiceClient may be used to make it more elegant and reliable
-    time.sleep(15)
+    print("Waiting for ATLAS to open")
+
+    # set up multiprocessing.Lock to be use to block until client is connected.
+    connect = threading.Lock()
+    connect.acquire()
+
+    def client_connected(client_name):
+        # event handler for OnClientConnected
+        print('\nATLAS Client connected.')
+        print(f'ATLAS version: {app.GetVersion()}')
+        connect.release()
+
+    app.OnClientConnected += client_connected
+    app.Connect(Path.GetFileNameWithoutExtension(AppDomain.CurrentDomain.FriendlyName));
+    
+    # Wait until client is connected. The next line cannot run until connect is released by the handeler function.
+    connect.acquire()
+    connect.release()
+
 
     setsList = workbookServiceClient.GetSets()
-    # Loading session from SQL Race into specified set, using session keys found in SQLRace API and connection string to SQL Race
-    app.LoadSqlRaceSessions(setsList[0].Id, keys, connection_strings)
     
-    # Waiting for session(s) to load
-    time.sleep(15) # here, instead of sleep, callbacks from Automation API MAT.Atlas.Automation.Client.Services.ApplicationServiceClient may be used to make it more elegant and reliable
+    load = threading.Lock()
+    load.acquire()
+
+    def session_loaded(session_loaded):
+        # event handeler for OnSessionLoaded
+           print('Session loaded.')
+           load.release()
+    app.OnSessionLoaded += session_loaded
+
+    # Loading session from SQL Race into specified set, using session keys found in SQLRace API and connection string to SQL Race
+    print('\nLoading session...')
+    app.LoadSqlRaceSessions(setsList[0].Id, keys, connection_strings)
+
+    # wait for session to load
+    load.acquire()
+    load.release()
 
     # Getting through the APIs to get the already loaded session ID 
     sessionServiceClient = SessionServiceClient()
@@ -192,5 +225,6 @@ def main():
     find_sessions()
 
     set_cursor(keys, connection_strings, 47718380000000)
+    
 
 if __name__ ==  "__main__": main()
