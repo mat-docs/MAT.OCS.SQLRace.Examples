0380133ebc25fce47ed20efb06902f899cd34b2e
wei-mclarenapplied <71443454+wei-mclarenapplied@users.noreply.github.com>
2023-08-16 17:01:18 +0100

    Update to SDK style projects and Mesl.Sqlrace.API nuget (#25)
    * Convert to sdk
    
    * "MESL.SQLRace.API" Version="2.1.23227.6"
    
    * update actions to use .net core
    
    * dotnet restore
    
    * remove adding source
    
    * remove included analzer files
    
    * Remove     <PackageReference Include="Baseclass.Contrib.Nuget.Output" Version="2.4.3" />
    
    * update readme
    
    ---------
    
    Co-authored-by: Wei Li <wei.li@mclaren.com>

--- MAT.SQLRace.LoadSessionFromDatabaseAndReadParameters/Program.cs
+++ MAT.SQLRace.LoadSessionFromDatabaseAndReadParameters/Program.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Linq;
-using System.ServiceModel.Configuration;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
