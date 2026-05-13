191e0dfac179cdd60276ddd6289def98ef22cf2e
KVNat1996 <karanvir.nat@mclarenapplied.com>
2026-05-13 08:34:42 +0100

    TimeZone Property SQL Race Example (#57)
    * 88880 - Addition of sample code to demonstrate implementation of the TimeZone property in sessions.
    
    * Forgot to add a commented out line calling the newly added CreateSessionWithTimeZoneValue() method
    
    * Corrected one console message in the TimeZone method example as well as replacing DateTime.Now with DateTime.UtcNow

--- MAT.SQLRace.HelloData/Program.cs
+++ MAT.SQLRace.HelloData/Program.cs
@@ -77,6 +77,7 @@ namespace MAT.SQLRace.HelloData
             //LoadLiveFunction();
             //WholeSessionsCompareMode();
             //TestMaxAndMinSampleIntervals();
+            //CreateSessionWithTimeZoneValue();
 
             Console.WriteLine("Press ENTER key to close.");
             Console.ReadLine();
@@ -232,6 +233,35 @@ namespace MAT.SQLRace.HelloData
             //session.AssociateWithParent(/*Parent session key*/);
 
             clientSession.Close();
+        }
+
+        private static void CreateSessionWithTimeZoneValue()
+        {
+            ConnectionString = $@"DbEngine=SQLite;Data Source=c:\ssn2\test01.ssn2;";
+
+            var sessionManager = SessionManager.CreateSessionManager();
+
+            var clientSession1 = sessionManager.CreateSessionWithTeamId(
+                ConnectionString,
+                SessionKey.NewKey(),
+                "clientSession1",
+                DateTime.UtcNow,
+                45,
+                "Session",
+                "UTC+00:00");
+
+            var clientSession2 = sessionManager.CreateSession(
+                ConnectionString,
+                SessionKey.NewKey(),
+                "clientSession2",
+                DateTime.UtcNow,
+                "Session");
+
+            var session1 = clientSession1.Session;
+            var session2 = clientSession2.Session;
+
+            Console.WriteLine($"Session one created with timezone: {session1.TimeZone}");
+            Console.WriteLine($"Session two created with timezone: {session2.TimeZone}");
         }
 
         private static void TestMaxAndMinSampleIntervals()
